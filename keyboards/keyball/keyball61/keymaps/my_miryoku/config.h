8bb51d64648a2596f84bcf5906a912957849837b
Jonathan Huizingh <Jonathan@Huizingh.info>
2025-08-17 15:14:41 -0700

    Higher CPI for mouse movement on reboot

--- keyboards/keyball/keyball61/keymaps/my_miryoku/config.h
+++ keyboards/keyball/keyball61/keymaps/my_miryoku/config.h
@@ -37,6 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
+#define KEYBALL_CPI_DEFAULT 1100
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
